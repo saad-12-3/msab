5e660282e47673999a2d3602c96b3175a0661f84
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-03-18 13:57:48 +0600

    enhance login page UI with logo and updated button text; add empty register page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,13 +2,13 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Image from "next/image";
 import Link from "next/link";
 
 const Page = () => {
@@ -17,26 +17,42 @@ const Page = () => {
       <div className="">
         <Card className="w-[350px]">
           <CardHeader>
-            <CardTitle>লগইন করুন</CardTitle>
-            <CardDescription>আপনার অ্যাকাউন্টে প্রবেশ করুন</CardDescription>
+            {/* <CardTitle>লগইন করুন</CardTitle> */}
+            <div className="flex justify-center gap-5 items-center flex-col">
+              <Image src="/asset/msab_logo_b.png" width={85} height={85} alt={"msab logo"} />
+              <CardTitle>আপনার অ্যাকাউন্টে প্রবেশ করুন</CardTitle>
+            </div>
           </CardHeader>
           <CardContent>
             <form>
               <div className="grid w-full items-center gap-4">
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="emailPhone">ইমেইল বা ফোন নম্বর</Label>
-                  <Input id="emailPhone" placeholder="আপনার ইমেইল বা ফোন নম্বর লিখুন" />
+                  <Input
+                    id="emailPhone"
+                    placeholder="আপনার ইমেইল বা ফোন নম্বর লিখুন"
+                  />
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="password">পাসওয়ার্ড</Label>
-                  <Input id="password" type="password" placeholder="আপনার পাসওয়ার্ড লিখুন" />
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="আপনার পাসওয়ার্ড লিখুন"
+                  />
                 </div>
               </div>
             </form>
           </CardContent>
           <CardFooter className="flex justify-between">
-            <Link href={"/"}><Button variant="outline" className="text-red-700">বাতিল</Button></Link>
-            <Button type="submit" className="bg-green-700 hover:bg-green-500">লগইন</Button>
+            <Link href={"/"}>
+              <Button variant="outline" className="text-red-700">
+                বাতিল করুন
+              </Button>
+            </Link>
+            <Button type="submit" className="bg-green-700 hover:bg-green-500">
+              প্রবেশ করুন
+            </Button>
           </CardFooter>
         </Card>
       </div>
