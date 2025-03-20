cf16563fbe41c8dfb1072305dc971d800487cc92
saad-12-3 <mosayebhossain.o@gmail.com>
2025-03-20 10:47:42 +0600

    creating footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/pages/home/footer";
 import Hero from "@/pages/home/hero";
 import Services from "@/pages/home/services";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div >
       <Hero></Hero>
       <Services></Services>
+      <Footer></Footer>
     </div>
   );
 }
