8215d75ad022eecc3b309acc85a148daf8c7dc0c
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-03-20 15:30:06 +0600

    feat: restructure layout with header and footer components, enhance header functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,17 @@
-
-import Footer from "@/pages/home/footer";
+import AboutSection from "@/pages/aboutSection";
 import FaqSection from "@/pages/home/FaqSection";
 import Hero from "@/pages/home/hero";
 import LatestBlog from "@/pages/home/LatestBlog";
 import Services from "@/pages/home/services";
-import AboutSection from "@/pages/aboutSection";
-
 
 export default function Home() {
   return (
-    <div >
-      <Hero/>
-      
-      <Services/>
-      <FaqSection/>
-      <LatestBlog/>
+    <div>
+      <Hero />
+      <Services />
+      <FaqSection />
+      <LatestBlog />
       <AboutSection></AboutSection>
-      <Footer/>
     </div>
   );
 }
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,57 +1,81 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { IoHome, IoSearch } from "react-icons/io5";
 import { Input } from "../ui/input";
 
 const Header: React.FC = () => {
+  const path = usePathname();
+  const [action, setActive] = useState(true);
+
+  console.log(path , action);
+
+  useEffect(() => {
+    if (path === "/auth/login" || path === "/auth/register") {
+      setActive(false);
+    }else{
+      setActive(true)
+    }
+  }, [path]);
+
   return (
     <div className="bg-green-700">
       <header className="w-11/12 mx-auto md:w-10/12 py-4 ">
-      <div className="flex justify-between items-center">
-        {/* Logo */}
-        <div className="flex items-center">
-          <Image
-            className="bg-white rounded-md"
-            src={"/asset/msab_logo_b.png"}
-            width={75}
-            height={75}
-            alt="MSAB Logo"
-          />
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link href={"/"}>
+              <Image
+                className="bg-white rounded-md"
+                src={"/asset/msab_logo_b.png"}
+                width={75}
+                height={75}
+                alt="MSAB Logo"
+              />
+            </Link>
+          </div>
+
+          {/* Navigation */}
+          <nav className="hidden md:flex gap-6">
+            <NavItem
+              href="/"
+              icon={<IoHome className="text-red-700" />}
+              text="হোম"
+            />
+            <NavItem href="#" text="সেবাসমূহ" />
+            <NavItem href="#" text="ব্লগ" />
+            <NavItem href="#" text="যোগাযোগ" />
+          </nav>
+
+          {/* Login Button */}
+          <Link href="/auth/login">
+            <button className="px-4 py-2 rounded-md text-white bg-red-700 hover:bg-green-700 transition-all">
+              Login
+            </button>
+          </Link>
         </div>
 
-        {/* Navigation */}
-        <nav className="hidden md:flex gap-6">
-          <NavItem href="/" icon={<IoHome className="text-red-700" />} text="হোম" />
-          <NavItem href="#" text="সেবাসমূহ" />
-          <NavItem href="#" text="ব্লগ" />
-          <NavItem href="#" text="যোগাযোগ" />
-        </nav>
-
-        {/* Login Button */}
-        <Link href="/auth/login">
-          <button className="px-4 py-2 rounded-md text-white bg-red-700 hover:bg-green-700 transition-all">
-            Login
-          </button>
-        </Link>
-      </div>
-
-      {/* Search Bar */}
-      <div className="py-10 flex justify-center">
-        <div className="relative w-full max-w-lg">
-          <Input
-            type="text"
-            className="w-full py-6 pl-5 pr-16 rounded-full bg-white/70 shadow-lg backdrop-blur-md focus:ring-2 focus:ring-green-500 outline-none transition-all"
-            placeholder="আপনি কি খুঁজছেন?"
-          />
-          <button
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-green-600 text-white p-2 rounded-full hover:bg-red-700 transition-all"
-            aria-label="Search"
-          >
-            <IoSearch className="text-xl" />
-          </button>
+        {/* Search Bar */}
+        {action && (
+          <div className="py-10 flex justify-center">
+          <div className="relative w-full max-w-lg">
+            <Input
+              type="text"
+              className="w-full py-6 pl-5 pr-16 rounded-full bg-white/70 shadow-lg backdrop-blur-md focus:ring-2 focus:ring-green-500 outline-none transition-all"
+              placeholder="আপনি কি খুঁজছেন?"
+            />
+            <button
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-green-600 text-white p-2 rounded-full hover:bg-red-700 transition-all"
+              aria-label="Search"
+            >
+              <IoSearch className="text-xl" />
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+        )}
+      </header>
     </div>
   );
 };
